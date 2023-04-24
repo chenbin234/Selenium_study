63cd53af4f45a28bf5f7f66cbb8ce734501c3cba
chenbingcheng <bingcheng.chen@outlook.com>
2023-04-24 22:18:29 +0200

    update

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
 from selenium import webdriver
+import time
 
+drivers = webdriver.Chrome()
+drivers.get('https://www.baidu.com')
+
+# Find the search bar and enter text
+search_box = drivers.find_element('id','kw')
+search_box.send_keys("dogs pictures")
+
+# Find the search button and click it
+search_button = drivers.find_element('id','su')
+search_button.click()
+
+
+time.sleep(20)
+drivers.quit()
